b4907dde412502abd2b8bcff51ddb7ede8b73694
Jaclyn Hall <jaclynchall@gmail.com>
2018-07-13 19:13:50 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -72,8 +72,9 @@ class TicTacToe
   def won?
     WIN_COMBINATIONS.each do |wincombo|
       if @board[wincombo[0]] == "X" && @board[wincombo[1]] == "X" && @board[wincombo[2]] == "X"
-        return wincombo
+        return wincombo.array
       elsif @board[wincombo[0]] == "O" && @board[wincombo[1]] == "O" && @board[wincombo[2]] == "O"
+        return wincombo.array
       else
         return false
       end
